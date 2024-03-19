66c0af189e35a685c8b0a8480f4dbd58df37804b
megumii <contact@megumii.xyz>
2024-03-19 22:43:31 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-09661d4a-3595-41e6-81ff-1bca339bbe30/)
 
 
 echo "$curl_output" >> Update.tsx
