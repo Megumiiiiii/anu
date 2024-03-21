24ebfd4af5a92cec35865913cab62f8f726d159f
Megumiiiiii <contact@megumii.xyz>
2024-03-21 03:31:58 +0000

    🟤 auto commit

--- Update.tsx
+++ Update.tsx
@@ -137,3 +137,4 @@
 {"error":"{\"Error_GUID\":\"9749732387224641835\",\"Error\":\"Failed all retries {GUID:9749732387224641835,error:No pairings available.,chain_id:ETH1}\"}"}
 {"error":"{\"Error_GUID\":\"3107949574189199590\",\"Error\":\"Failed all retries {GUID:3107949574189199590,error:No pairings available.,chain_id:ETH1}\"}"}
 {"error":"{\"Error_GUID\":\"8169244745976596486\",\"Error\":\"Failed all retries {GUID:8169244745976596486,error:No pairings available.,chain_id:ETH1}\"}"}
+{"error":"{\"Error_GUID\":\"265448504421270198\",\"Error\":\"Failed all retries {GUID:265448504421270198,error:No pairings available.,chain_id:ETH1}\"}"}
